db7e50ce551ae11092ce61e07119c8573d340f35
chrizxzayax <chmolina629@hotmail.com>
2025-10-26 21:54:58 -0700

    attempting for 7th time to fix the menu display options

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 #include "Goat.h"
 using namespace std;
 
@@ -84,15 +87,27 @@ int main() {
 
 int main_menu() {
     // start making th display menu and get user choice
-    int choice;
-    cout << "\nGoat Trip Menu\n";
+    int choice = 0;
+    while(true){
+        cout << "\nGoat Trip Menu\n";
     cout << "1. Add a goat to the trip\n";
     cout << "2. Delete a goat from the trip\n";
     cout << "3. Display the trip goats\n";
     cout << "4. Exit\n";
     cout << "Enter your choice: ";
-    cin >> choice;
+    if (!(cin >> choice)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+         cout << "Please enter a number between 1 and 4.\n";
+            continue;
+        }
+        if (choice < 1 || choice > 4) {
+            cout << "Please enter a number between 1 and 4.\n";
+            continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return choice;
+    }
 }
 
 void add_goat(list<Goat> &trip, string names[], int nameCount, string colors[], int colorCount) {
@@ -107,8 +122,34 @@ void add_goat(list<Goat> &trip, string names[], int nameCount, string colors[], 
     
     string name = names[nis];
     string color = colors[cis];
-    Goat newGoat(name, age, color);
-    trip.push_back(newGoat);
+    Goat g(name, age, color);
+    trip.push_back(g);
 
-    cout << "Added goat: " << name << ", Age: " << age << ", Color: " << color << endl;
+    cout << "Added goat: " << g << "\n";
+
+}
+
+//to display numbered list and return index of selected goat
+int select_goat(const list<Goat>& trip) {
+    if (trip.empty()) {
+        cout << "No goats available to select.\n";
+        return 0;
+    }
+
+    cout << "Select a goat from the list:\n";
+    int index = 1;
+    for (const Goat &g  : trip) {
+        cout << "    [" << index << "] " << g << "\n";
+        ++index;
+    }
+
+    int choice;
+    cout << "Select a goat by number (or 0 to cancel): ";
+    cin >> choice;
+
+    if (choice < 0 || choice >= index) {
+        cout << "Invalid selection.\n";
+        return -1;
+    }
+    return choice - 1; // return zero-based index
 }
